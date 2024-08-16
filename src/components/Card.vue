cdeaa4e287e0288ae0df86f533134f5f59b2b08f
Mardoqueiro <mordecai.dimbu@gmail.com>
2024-08-16 14:44:09 +0200

    cardSlot

---
+++ src/components/Card.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="card">
+    <div class="card-header">
+        <slot name="cardHeader"></slot>
+    </div>
+    <div class="card-body">
+        <slot name="cardBody"></slot>
+    </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+      name: "CardComp"
+  }
+  </script>
+  
+  <style scoped> 
+  .card {
+    width: 18rem;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 0 10px #F5CB86;
+    background-color: #17182E;
+  }
+
+  </style>
