28276f4af0195360b1aee470836fe096d7bf8a15
Mardoqueiro <mordecai.dimbu@gmail.com>
2024-08-17 09:04:36 +0200

    navBarandFooter

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="container-fluid">
     <div class="row">
-      <h2 class="display-2">Welcome to store </h2>
+      <h2 class="display-2">Welcome to our store</h2>
     </div>
+    
+
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'HomeView',
-
   components: {
   }
 }
