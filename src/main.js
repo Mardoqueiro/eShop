5399110e746de4a0cdac39ca25b26ba1ab301333
Mardoqueiro <mordecai.dimbu@gmail.com>
2024-08-17 13:46:13 +0200

    recentProducts

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist'
+import 'bootstrap'
 
 createApp(App).use(store).use(router).mount('#app')
